023e7771ed8f7b16f6d6db6f4a00d601351731ad
Pramod Kotipalli <pramod.kotipalli@gmail.com>
2016-05-18 00:31:30 -0700

    Initial Video creation and views

--- richcaptions/player/models.py
+++ richcaptions/player/models.py
@@ -7,13 +7,13 @@ class Video(models.Model):
     """
 
     # The URL of the YouTube video (e.g. https://www.youtube.com/watch?v=JC82Il2cjqA)
-    url = models.URLField(blank=False)
+    url = models.URLField(blank=False, verbose_name="video URL")
 
     # The YouTube video id (e.g. JC82Il2cjqA)
-    resource_id = models.CharField(max_length=16, blank=False)
+    resource_id = models.CharField(max_length=16, blank=False, verbose_name="video resource ID")
 
     # Some metadata attributes of the video
-    title = models.CharField(max_length=64, blank=True)
+    title = models.CharField(max_length=64, blank=True, verbose_name="video title")
 
     def __str__(self):
         if self.title != '':
