f75b277eba160c342d7bd3195a40db9dea411ef6
Pramod Kotipalli <pramod.kotipalli@gmail.com>
2016-05-19 20:23:30 -0700

    Deploy to Heroku

--- richcaptions/richcaptions/settings.py
+++ richcaptions/richcaptions/settings.py
@@ -46,8 +46,6 @@ INSTALLED_APPS = [
     'home.apps.HomeConfig',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
-
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -92,6 +90,10 @@ DATABASES = {
     }
 }
 
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
@@ -129,7 +131,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 
 # Django REST framework settings
 REST_FRAMEWORK = {
@@ -140,3 +151,12 @@ REST_FRAMEWORK = {
     ],
     'PAGE_SIZE': 1000,
 }
+
+# CORS settings
+CORS_ORIGIN_ALLOW_ALL = True
+
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
