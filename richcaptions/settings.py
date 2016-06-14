ae681c555a815953b73b38ec9ee8e8a43de7da0f
Pramod Kotipalli <pramod.kotipalli@gmail.com>
2016-06-13 21:51:01 -0400

    Turn on DEBUG

--- richcaptions/settings.py
+++ richcaptions/settings.py
@@ -23,7 +23,7 @@ PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'rich-captions.herokuapp.com', 'rc.pramodk.net']
 
